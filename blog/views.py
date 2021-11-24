07b46e4bbb338585a91484e687db241749201307
d.selyanina <daraselanina89009@gmail.com>
2021-11-24 19:44:43 +0300

    добавила апи

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AuthorSerializer
+from .models import Author
 
 # Create your views here.
+
+
+class AuthorAPI(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
