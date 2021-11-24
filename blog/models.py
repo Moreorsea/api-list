03ca00521b3a46bd50322d5625581edb5bed97ed
d.selyanina <daraselanina89009@gmail.com>
2021-11-24 20:33:38 +0300

    добавила новые апи

--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,39 @@ class Author(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('died', null=True, blank=True)
 
+    class Meta:
+        verbose_name = 'Автора'
+        verbose_name_plural = 'Авторы книг'
+
     def get_absolute_url(self):
         return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
         return '%s, %s' % (self.last_name, self.first_name)
+
+
+class Genre(models.Model):
+    name = models.CharField('Название жанра', max_length=100)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры книг'
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField('Название книги', max_length=100)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name='book_author', verbose_name='Автор')
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE,
+                              related_name='book_genre', verbose_name='Жанр книги')
+    description = models.TextField('Описание книги', max_length=1000)
+
+    class Meta:
+        verbose_name = 'Книгу'
+        verbose_name_plural = 'Книги'
+
+    def __str__(self):
+        return self.name
