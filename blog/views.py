2874c219e404a48a41d1d0c9e9e582f60c3370cd
d.selyanina <daraselanina89009@gmail.com>
2021-12-05 21:46:29 +0300

    правки по корс

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,32 @@
+# import git
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
 from .serializers import AuthorSerializer, GenreSerializer, BookSerializer
 from .models import Author, Genre, Book
+# from git import Repo
 
 # Create your views here.
+
+
+@csrf_exempt
+def update(request):
+    if request.method == "POST":
+        '''
+        pass the path of the diectory where your project will be
+        stored on PythonAnywhere in the git.Repo() as parameter.
+        Here the name of my directory is "test.pythonanywhere.com"
+        '''
+        repo = git.Repo("moreorsea.pythonanywhere.com/")
+        origin = repo.remotes.origin
+
+        origin.pull()
+
+        return HttpResponse("Updated code on PythonAnywhere")
+    else:
+        return HttpResponse("Couldn't update the code on PythonAnywhere")
 
 
 class AuthorAPI(viewsets.ModelViewSet):
