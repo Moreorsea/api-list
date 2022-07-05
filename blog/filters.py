ca1d81f12ae6e107063f7d53c97c58960f9a363a
d.selyanina <daraselanina89009@gmail.com>
2022-07-05 19:00:11 +0300

    обновила фильтр

--- blog/filters.py
+++ blog/filters.py
@@ -1,9 +1,13 @@
 import django_filters
+from django_filters.widgets import RangeWidget
 
 from .models import Flat
 
 
 class FlatFilter(django_filters.rest_framework.FilterSet):
+    price = django_filters.DateFromToRangeFilter(widget=RangeWidget(attrs={'type': 'text'}))
+    square = django_filters.DateFromToRangeFilter(widget=RangeWidget(attrs={'type': 'text'}))
+
     class Meta:
         model = Flat
         fields = ['price', 'count_room', 'square']
